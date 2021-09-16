cfa248613c536810772af23f5a3ab45bfe21f22c
Beto Garcia <bookstylus@gmail.com>
2021-09-16 08:36:22 -0300

    Init Nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.setAppHeight()
+    window.addEventListener('resize', this.setAppHeight)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.setAppHeight)
+  },
+  methods: {
+    // get viewport height
+    setAppHeight() {
+      const doc = document.documentElement
+      doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+:root {
+  --app-height: 100%;
+}
+
+html,
+body,
+.inner-h {
+  padding: 0;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  height: var(--app-height);
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,22 @@ export default {
   head: {
     title: 'book',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,13 +30,77 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/pwa',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ['@nuxt/http', '@nuxtjs/sitemap'],
 
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+  },
+  googleFonts: {
+    families: {
+      Oswald: {
+        wght: ['200..700'],
+      },
+      Rubik: {
+        wght: ['300..900'],
+        ital: ['300..900'],
+      },
+      'Playfair+Display': {
+        wght: ['400..900'],
+      },
+      'Amatic+SC': {
+        wght: ['700'],
+      },
+      'Mr+De+Haviland': true,
+      'Material+Icons+Round': true,
+    },
+    display: 'optional',
+    download: true,
+    useStylesheet: false,
+  },
+  sitemap: {
+    hostname: 'https://bookstylus.com',
+  },
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    meta: {
+      name: 'ConvitesOn',
+      description:
+        'Seu convite tá ON📲 🎉Compartilhe sua vitória em grande estilo.',
+      lang: 'pt-BR',
+      theme_color: '#000',
+    },
+    manifest: {
+      name: 'ConvitesOn',
+      short_name: 'ConvitesOn',
+      description:
+        'Seu convite tá ON📲 🎉Compartilhe sua vitória em grande estilo.',
+      lang: 'pt-BR',
+      theme_color: '#000',
+      background_color: '#fff',
+      orientation: 'portrait',
+      start_url: '/?standalone=true',
+      scope: '/',
+      display: 'standalone',
+    },
+    workbox: {
+      offlineStrategy: 'cacheFirst',
+      runtimeCaching: [
+        {
+          urlPattern: 'https://fonts.googleapis.com/',
+          handler: 'cacheFirst',
+        },
+        {
+          urlPattern: 'https://fonts.gstatic.com/',
+          handler: 'cacheFirst',
+        },
+      ],
+    },
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
