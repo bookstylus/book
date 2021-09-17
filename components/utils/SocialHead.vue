720c81325cdd3e94e27fc6d70b91931bccede52c
Beto Garcia <bookstylus@gmail.com>
2021-09-17 09:36:40 -0300

    Update SocialHead.vue

--- components/utils/SocialHead.vue
+++ components/utils/SocialHead.vue
@@ -7,32 +7,10 @@
 // Test on: https://developers.facebook.com/tools/debug/
 export default {
   props: {
-    base: {
-      type: String,
+    social: {
+      type: Object,
       required: true,
     },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      default: '',
-    },
-  },
-  jsonld() {
-    return {
-      '@context': 'http://schema.org',
-      '@type': 'article',
-      name: `ConvitesOn`,
-      url: `${this.$config.publicURL}${this.$route.path}`,
-      description: this.description,
-      image: `${this.$config.publicURL}${this.image}`,
-    }
   },
   head() {
     return {
@@ -45,27 +23,32 @@ export default {
         {
           hid: 'twitter:site',
           property: 'twitter:site',
-          content: '@/conviteson',
+          content: `@/${this.social.twitter}`,
+        },
+        {
+          hid: 'twitter:creator',
+          property: 'twitter:creator',
+          content: `@/${this.social.twitter}`,
         },
         {
           hid: 'twitter:title',
           name: 'twitter:title',
-          content: this.title,
+          content: this.social.title,
         },
         {
           hid: 'twitter:description',
           name: 'twitter:description',
-          content: this.description,
+          content: this.social.description,
         },
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: `${this.$config.publicURL}${this.image}`,
+          content: this.social.photo,
         },
         {
           hid: 'twitter:image:alt',
           name: 'twitter:image:alt',
-          content: this.title,
+          content: this.social.title,
         },
         {
           hid: 'fb:app_id',
@@ -80,37 +63,128 @@ export default {
         {
           hid: 'og:title',
           property: 'og:title',
-          content: this.title,
+          content: this.social.title,
         },
         {
           hid: 'og:description',
           property: 'og:description',
-          content: this.description,
+          content: this.social.description,
         },
         {
           hid: 'og:site_name',
           property: 'og:site_name',
-          content: 'ConvitesOn',
+          content: this.social.sitename,
         },
         {
           hid: 'og:url',
           property: 'og:url',
-          content: `${this.$config.publicURL}${this.$route.path}`,
+          content: `${this.$config.baseURL}${this.$route.path}`,
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `${this.$config.publicURL}${this.image}`,
+          content: this.photo,
         },
         {
           hid: 'og:image:secure_url',
           property: 'og:image:secure_url',
-          content: `${this.$config.publicURL}${this.image}`,
+          content: this.photo,
         },
         {
           hid: 'og:image:alt',
           property: 'og:image:alt',
-          content: this.title,
+          content: this.social.title,
+        },
+      ],
+    }
+  },
+  computed: {
+    photo1x1() {
+      const width = 1200
+      const height = 1200
+      const img = this.$img(this.image, {
+        width,
+        height,
+        quality: 70,
+        format: 'webp',
+      })
+      return `${this.$config.baseURL}${img}`
+    },
+    photo4x3() {
+      const width = 1200
+      const height = 900
+      const img = this.$img(this.image, {
+        width,
+        height,
+        quality: 70,
+        format: 'webp',
+      })
+      return `${this.$config.baseURL}${img}`
+    },
+    photo3x4() {
+      const width = 1200
+      const height = 1600
+      const img = this.$img(this.image, {
+        width,
+        height,
+        quality: 70,
+        format: 'webp',
+      })
+      return `${this.$config.baseURL}${img}`
+    },
+    photo() {
+      const width = 1200
+      const height = 600
+      const img = this.$img(this.image, {
+        width,
+        height,
+        quality: 70,
+        format: 'webp',
+      })
+      return `${this.$config.baseURL}${img}`
+    },
+  },
+  jsonld() {
+    return {
+      '@context': 'http://schema.org',
+      '@graph': [
+        {
+          '@type': 'NewsArticle',
+          mainEntityOfPage: {
+            '@type': 'WebPage',
+            '@id': `${this.$config.baseURL}${this.$route.path}`,
+          },
+          headline: this.social.title,
+          description: this.social.description,
+          image: [this.photo1x1, this.photo4x3, this.photo3x4],
+          datePublished: new Date(),
+          dateModified: new Date(),
+          author: {
+            '@id': `${this.$config.baseURL}/#Autor`,
+          },
+          publisher: {
+            '@type': 'Organization',
+            name: 'ConvitesOn',
+            logo: {
+              '@type': 'ImageObject',
+              url: 'https://google.com/logo.jpg',
+            },
+            url: 'https://conviteson.com/',
+            sameAs: [
+              'https://www.facebook.com/convitesoncom/',
+              'https://www.instagram.com/convitesoncom/',
+              'https://g.page/conviteson?share',
+              'https://twitter.com/conviteson',
+            ],
+          },
+        },
+        {
+          '@type': 'Autor',
+          '@id': `${this.$config.baseURL}/#Autor`,
+          name: this.social.name,
+          image: this.doctorphoto1x1,
+          url: `${this.$config.baseURL}${this.$route.path}`,
+          sameAs: this.social.socials,
         },
       ],
     }
